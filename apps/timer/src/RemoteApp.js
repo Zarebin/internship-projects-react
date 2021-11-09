8e357267dbe93bf0a90ea6641986d40d88390078
Hassan Ketabi <ketabih@chmail.ir>
2021-11-09 11:07:46 +0330

    Add microfrontend configs for timer

---
+++ apps/timer/src/RemoteApp.js
@@ -0,0 +1,12 @@
+import React from "zarkit/react";
+import { Button } from "zarkit/Button"
+
+function RemoteApp() {
+    return (
+        <div> من یک شمارنده بامزه هستم
+            <Button />
+        </div>
+    )
+}
+
+export default RemoteApp;
---
+++ apps/timer/src/bootstrap.js
@@ -0,0 +1,7 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from 'react-dom'
+
+// import ReactDOM from "lib-app/react-dom";
+// import React from "lib-app/react";
+ReactDOM.render(<App />, document.getElementById("app"));
