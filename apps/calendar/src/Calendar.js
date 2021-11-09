2760c7b6db11b66a6644c51496d661597ad3f23d
behradhajmiri <behradhajmiri@gmail.com>
2021-11-09 22:29:59 +0330

    [calendar] setting up React and MicroFrontEnd for calendar project

---
+++ apps/calendar/src/Calendar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Calendar() {
+   return (
+      <div>Calendar App Behrad</div>
+   )
+}
+
+export default Calendar;
---
+++ apps/calendar/src/bootstrap.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import Calendar from "./Calendar";
+
+
+ReactDOM.render(<Calendar />, document.getElementById("calendar-app"));
