47c19656b8cb9da3dde1571479f27400e00391ff
sarasadatnasr <sarasadatnasr@gmal.com>
2021-11-13 18:23:34 +0330

    setting up microfrontend and react for Spinner

---
+++ apps/spinner/src/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Spinner() {
+    return ( 
+        <div> Chance-wheel and Spinner</div>
+    )
+}
+export default Spinner;
---
+++ apps/spinner/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import Spinner from './Spinner'
+
+ReactDOM.render( < Spinner / > , document.getElementById("Converter"));
