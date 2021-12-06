9c5079b5d55b963389ffdeea7a8981944f0ed87f
zahra ghorbani <z.ghorbani476@gmail.com>
2021-12-06 22:07:11 +0330

    add temperature in object data

--- apps/converter/Components/converterfunc.js
+++ apps/converter/Components/converterfunc.js
@@ -1,9 +1,11 @@
-import TemperatureFunc from './Temperature';
+
 import formule from '../data/formula';
 
 function Converterfunc(topselect, val, madeselection1, madeselection2) {
   let result;
-  if (madeselection1 === madeselection2) { result = val * 1; } else if (topselect === 'temperature') { result = TemperatureFunc(val, madeselection1, madeselection2); } else {
+  if (madeselection1 === madeselection2) { result = val * 1; } 
+  else if (topselect === 'temperature') { result = formule[topselect][madeselection1][madeselection2](val) } 
+  else {
     result = val * (formule[topselect][madeselection1] / formule[topselect][madeselection2]);
   }
   return result;
