6b6eb169e25a9a2de4db182c866c057c76f960ca
sarasadatnasr <sarasadatnasr@gmal.com>
2021-12-14 22:35:39 +0330

    "spinner App"

---
+++ apps/spinner/src/bootstrap.jsx
@@ -0,0 +1,11 @@
+import React from 'zarkit/react';
+import ReactDOM from 'zarkit/react-dom';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
